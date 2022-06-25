b9c70f022aca32fd1be54600c5d88f8bf7a75910
MartinCapocasale <martincapocasale@gmail.com>
2022-06-25 12:38:29 -0300

    se agregan los modelos de vuelos, hoteles y excursiones

--- ProyectoViajes/ProyectoViajesApp/models.py
+++ ProyectoViajes/ProyectoViajesApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vuelo(models.Model):
+
+    id_vuelo = models.IntegerField()
+    origen = models.CharField(max_length=30)
+    destino = models.CharField(max_length=30)
+    ida = models.DateTimeField()
+    vuelta = models.DateTimeField()
+    precio = models.FloatField()
+
+class Hotel(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    ubicacion = models.CharField(max_length=60)
+    descripcion = models.CharField(max_length=150)
+    desde = models.DateTimeField()
+    hasta = models.DateTimeField()
+    precio = models.FloatField()
+
+class Excursion(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    ubicacion = models.CharField(max_length=60)
+    descripcion = models.CharField(max_length=150)
+    duracion = models.DateTimeField()
+    precio = models.FloatField()
