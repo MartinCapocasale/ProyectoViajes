904c982bd933b915979ecb88c743fba8e3ce3a26
MartinCapocasale <martincapocasale@gmail.com>
2022-06-26 10:17:20 -0300

    se agrega la funcion crear_hotel en la vista

--- ProyectoViajes/ProyectoViajesApp/views.py
+++ ProyectoViajes/ProyectoViajesApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 
@@ -22,4 +22,25 @@ def hoteles(request):
 
 def excursiones(request):
     excursiones = Excursion.objects.all()
-    return render(request,"ProyectoViajesApp/excursiones.html",{"excursiones":excursiones})    
+    return render(request,"ProyectoViajesApp/excursiones.html",{"excursiones":excursiones})
+
+def crear_hotel(request):
+    #Post
+    if request.method == "POST":
+        formulario = NuevoHotel(request.POST)
+
+        if formulario.is_valid():
+            info_hotel = formulario.cleaned_data
+
+            hotel = Hotel(nombre = info_hotel["nombre"],ubicacion = info_hotel["ubicacion"],descripcion = info_hotel["descripcion"],desde = info_hotel["desde"],hasta = info_hotel["hasta"],precio = int(info_hotel["precio"]))
+            
+            hotel.save()#guardamos en la bdd
+            
+            return redirect("hoteles")#te redirecciono a la pagina de inicio
+        else:
+            return render(request,"ProyectoViajesApp/formulario_hotel.html",{"form":formularioVacio})
+
+    #Get y otros
+    else:
+        formularioVacio = NuevoHotel()
+        return render(request,"ProyectoViajesApp/formulario_hotel.html",{"form":formularioVacio})
